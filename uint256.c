962fd77a1f7f7984de2ae397f20933c5f7ca305c
Saathik Chandupatla <saathvik.chandupatla@gmail.com>
2023-09-02 14:03:44 -0400

    finished and tested get_bits function; finished ms1

--- uint256.c
+++ uint256.c
@@ -7,6 +7,7 @@
 // Create a UInt256 value from a single uint32_t value.
 // Only the least-significant 32 bits are initialized directly,
 // all other bits are set to 0.
+// MS1
 UInt256 uint256_create_from_u32(uint32_t val) {
   UInt256 result;
   for (int i = 0; i < 8; i++) {
@@ -22,6 +23,7 @@ UInt256 uint256_create_from_u32(uint32_t val) {
 // Create a UInt256 value from an array of NWORDS uint32_t values.
 // The element at index 0 is the least significant, and the element
 // at index 7 is the most significant.
+// MS1
 UInt256 uint256_create(const uint32_t data[8]) {
   UInt256 result;
   for (int i = 0; i < 8; i++) {
@@ -31,6 +33,7 @@ UInt256 uint256_create(const uint32_t data[8]) {
 }
 
 // Create a UInt256 value from a string of hexadecimal digits.
+// MS2
 UInt256 uint256_create_from_hex(const char *hex) {
   UInt256 result;
   // TODO: implement
@@ -39,6 +42,7 @@ UInt256 uint256_create_from_hex(const char *hex) {
 
 // Return a dynamically-allocated string of hex digits representing the
 // given UInt256 value.
+// MS2
 char *uint256_format_as_hex(UInt256 val) {
   char *hex = NULL;
   // TODO: implement
@@ -48,13 +52,15 @@ char *uint256_format_as_hex(UInt256 val) {
 // Get 32 bits of data from a UInt256 value.
 // Index 0 is the least significant 32 bits, index 7 is the most
 // significant 32 bits.
+// MS1
 uint32_t uint256_get_bits(UInt256 val, unsigned index) {
   uint32_t bits;
-  // TODO: implement
+  bits = val.data[index];
   return bits;
 }
 
 // Compute the sum of two UInt256 values.
+// MS2
 UInt256 uint256_add(UInt256 left, UInt256 right) {
   UInt256 sum;
   // TODO: implement
@@ -62,6 +68,7 @@ UInt256 uint256_add(UInt256 left, UInt256 right) {
 }
 
 // Compute the difference of two UInt256 values.
+// MS2
 UInt256 uint256_sub(UInt256 left, UInt256 right) {
   UInt256 result;
   // TODO: implement
@@ -69,6 +76,7 @@ UInt256 uint256_sub(UInt256 left, UInt256 right) {
 }
 
 // Return the two's-complement negation of the given UInt256 value.
+// MS2
 UInt256 uint256_negate(UInt256 val) {
   UInt256 result;
   // TODO: implement
@@ -78,6 +86,7 @@ UInt256 uint256_negate(UInt256 val) {
 // Return the result of rotating every bit in val nbits to
 // the left.  Any bits shifted past the most significant bit
 // should be shifted back into the least significant bits.
+// MS2
 UInt256 uint256_rotate_left(UInt256 val, unsigned nbits) {
   UInt256 result;
   // TODO: implement
@@ -87,6 +96,7 @@ UInt256 uint256_rotate_left(UInt256 val, unsigned nbits) {
 // Return the result of rotating every bit in val nbits to
 // the right. Any bits shifted past the least significant bit
 // should be shifted back into the most significant bits.
+// MS2
 UInt256 uint256_rotate_right(UInt256 val, unsigned nbits) {
   UInt256 result;
   // TODO: implement
