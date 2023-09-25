18bdec5061e45cab39153cc3a377a6061ffb6897
Saathik Chandupatla <saathvik.chandupatla@gmail.com>
2023-09-24 23:34:27 -0400

    updated trim non alpha, fully working still

--- csf_2/csf_assign02/c_wcfuncs.c
+++ csf_2/csf_assign02/c_wcfuncs.c
@@ -133,20 +133,13 @@ void wc_tolower(unsigned char *w) {  // done
 // NUL-terminated character string pointed-to by w.
 void wc_trim_non_alpha(unsigned char *w) { 
   int i = sizeof(w);
-  printf("\n%d\n", i);
-  printf("\n%d\n", w[i]);
-
-  while (w[i] == 0) {
+  while (w[i] == 0) { // get to the start of the non null characters in the string
     i--;
   }
-  
-  if ((w[i] < 65 || w[i] > 122)) {
-    while (i >= 0) {
-      printf("\n%d\n", w[i]);
-      if ((w[i] < 65 || w[i] > 122)) {
-        // printf("\n%d\n", w[i]);
-        w[i] = 0;
-      }
+   
+  if ((w[i] < 65 || w[i] > 122)) {  // only get rid of the non-alphabetic characters if they're at the end
+    while (i >= 0 && ((w[i] < 65 || w[i] > 122))) { // keep going until the string ends or hit a alphabetical character
+      w[i] = 0;
       i--;
     }
   }
